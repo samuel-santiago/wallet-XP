43c671d7f4b292ff246c0ea4cdad4364fe67b3ed
Samnas <samucasnascimento@hotmail.com>
2022-07-23 18:52:43 -0300

    feat: :goal_net: Implementing a styled Error Class in <helpers/Error.ts>
    	Creating styled error to send status|message to client
    
    * Creating an error middleware to be called after all the other routes in <server.ts>
    * Installing <express-assync-errors> package in order to capture the errors in the middleware
    
    Card 7

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
+import 'express-async-errors';
 import express from 'express';
+import errorMiddleware from './middlewares/errorMiddleware';
 import { router } from './routes';
 
 
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use(errorMiddleware);
 
 
 app.listen(PORT, ()=> console.log(`rodando na' ${PORT}`));
